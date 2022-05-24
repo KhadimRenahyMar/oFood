0d806619ff13fef21689cec296702d948fd64d50
nicolas-rouille <nicolas.rouille@laposte.net>
2022-05-24 20:33:54 +0200

    test deploiement

---
+++ client/src/components/Recipe/index.js
@@ -0,0 +1,38 @@
+import './styles.scss';
+import { useLocation } from 'react-router-dom';
+import sanitizeHtml from 'sanitize-html';
+
+export default function Recipe() {
+  const location = useLocation();
+  const data = location.state;
+  const { recipe } = data;
+  // console.log(recipe);
+  const steps = sanitizeHtml(recipe.steps_desc);
+  const ingredients = sanitizeHtml(recipe.ingredient_desc);
+  const intolerances = sanitizeHtml(recipe.intolerances);
+  console.log(steps);
+  console.log(ingredients);
+  console.log("Les intolerances :", recipe);
+
+  return (
+    <div className="recipe">
+      <div className="recipe__imgBx">
+
+        {/* <img src={`${recipe.photo_link}.png`} className="recipe__img" alt="" /> */}
+        
+        <img src={recipe.photo_link} className="recipe__img"></img>
+        <em className="recipe__time">{recipe.meal_time} minutes</em> {/* //test */}
+
+        <h2 className="recipe__title">{recipe.name}</h2> {/* //test */}
+
+        <p className="recipe__desc">Encore une bonne recette facile à préparer ? Merci oFood !</p> {/* //test */}
+
+      </div>
+
+      <div className="recipe__contentBx"> {/* //test */}
+        <div className="recipe__steps" dangerouslySetInnerHTML={{ __html: steps }} />
+        <div className="recipe__ingredients" dangerouslySetInnerHTML={{ __html: ingredients }} />
+      </div>
+    </div>
+  );
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
+// == Import : npm
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
+// == Import : local
+// Composants
+import App from 'src/components/App';
+
+// == Render
+// 1. Élément React racine (celui qui contient l'ensemble de l'app)
+//    => crée une structure d'objets imbriqués (DOM virtuel)
+const rootReactElement = (
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
+// 2. La cible du DOM (là où la structure doit prendre vie dans le DOM)
+const target = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(rootReactElement, target);
+
+
